8fbefb5e67fe17aecaaef10fb7b1908bbb1273ce
Tharinda Damruwan <85345427+tharindadamruwa@users.noreply.github.com>
2022-12-29 08:38:20 +0530

    update

--- index.php
+++ index.php
@@ -15,7 +15,6 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/res.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     
 </head>
 
